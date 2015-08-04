d8026ff4845bb7da3400b4d752b5b53e5a5cb85f
Stephen Florian <steezeven@gmail.com>
2015-08-04 16:03:38 -0600

    added html5 color picker feature

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 $(document).ready(function() {
   $('.pixel').click(function() {
     $(this).addClass('selected-pixel');
-    //$(this).css('background', newColor);
-    $('.color-palette').click(function() {
-      var newColor = $(this).css('background-color');
-      $('.selected-pixel').css('background-color', newColor);
-      $('.selected-pixel').removeClass('selected-pixel')
+    $(this).click(function(){
+      $(this).removeClass('selected-pixel');
     });
-      
   });
-  
+  $('#submit-button').click( function() {
+    newColor = $('#color-picker').val();
+    $('.selected-pixel').css('background-color', newColor);
+    $('.selected-pixel').removeClass('selected-pixel');
+  });
+  $('.color-palette').click(function() {
+    var newColor = $(this).css('background-color');
+    $('.selected-pixel').css('background-color', newColor);
+    $('.selected-pixel').removeClass('selected-pixel')
+  });
 });
+
+// $(document).ready(function() {
+//   $('.pixel').click(function() {
+//     $(this).addClass('selected-pixel');
+//     $(this).click(function(){
+//       $(this).removeClass('selected-pixel');
+//     });
+//     $('.color-palette').click(function() {
+//       var newColor = $(this).css('background-color');
+//       $('.selected-pixel').css('background-color', newColor);
+//       $('.selected-pixel').removeClass('selected-pixel')
+//     });
+      
+//   });
+  
+// });
+
 
 // $(document).ready(function() {
 //   $('.pixel').click(function() {
