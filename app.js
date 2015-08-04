e476043c97a9f19a655bceb03e0ecb90a5dbd512
Stephen Florian <steezeven@gmail.com>
2015-08-04 14:59:50 -0600

    made grid smaller, added jquery effects and clickable color palette

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
   $('.pixel').click(function() {
-    var newColor = prompt('What color do you want to change this to? Choose any color so long as it\'s black.');
-    $(this).css('background', newColor);
+    $(this).addClass('selected-pixel');
+    //$(this).css('background', newColor);
+    $('.color-palette').click(function() {
+      var newColor = $(this).css('background-color');
+      $('.selected-pixel').css('background-color', newColor);
+      $('.selected-pixel').removeClass('selected-pixel')
+    });
+      
   });
+  
 });
+
+// $(document).ready(function() {
+//   $('.pixel').click(function() {
+//     $(this).append('<input type="color" id="theButton" name="favcolor" value="#ffffff">');
+//     // $(this).css('background', $('#theButton').val());
+//     // $(#'theButton').remove();
+//   });
+//   $('body').focus(function(){
+//     $(this).css('background', $('#theButton').val());
+//     // $(#'theButton').remove();
+//   })
+// });
+
+
