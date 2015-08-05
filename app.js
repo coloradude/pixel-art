8139b859c1e775758296c642d5f6f03f7ab72608
Stephen Florian <steezeven@gmail.com>
2015-08-04 18:17:31 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -16,34 +16,3 @@ $(document).ready(function() {
     $('.selected-pixel').removeClass('selected-pixel')
   });
 });
-
-// $(document).ready(function() {
-//   $('.pixel').click(function() {
-//     $(this).addClass('selected-pixel');
-//     $(this).click(function(){
-//       $(this).removeClass('selected-pixel');
-//     });
-//     $('.color-palette').click(function() {
-//       var newColor = $(this).css('background-color');
-//       $('.selected-pixel').css('background-color', newColor);
-//       $('.selected-pixel').removeClass('selected-pixel')
-//     });
-      
-//   });
-  
-// });
-
-
-// $(document).ready(function() {
-//   $('.pixel').click(function() {
-//     $(this).append('<input type="color" id="theButton" name="favcolor" value="#ffffff">');
-//     // $(this).css('background', $('#theButton').val());
-//     // $(#'theButton').remove();
-//   });
-//   $('body').focus(function(){
-//     $(this).css('background', $('#theButton').val());
-//     // $(#'theButton').remove();
-//   })
-// });
-
-
